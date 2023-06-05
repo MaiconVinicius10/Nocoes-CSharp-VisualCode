d5eb61590886facb2e02d35077f4c337f016bac2
Maicon Vinicius <maicon.vinicius.77920@gmail.com>
2023-06-05 20:22:59 -0300

    Aula01
    Como escrever hello word na tela

---
+++ aula01/aula01.cs
@@ -0,0 +1,13 @@
+//aula01 aprenda a fala hello word
+// usando biblioteca do sistema
+using System;
+//criando classe
+class Principal{
+
+    // criando metodo 
+    static void Main(){
+        //exibindo hello word!
+        Console.WriteLine("Hello Word!!");
+    }
+}
+/// OBS: Para abrir pasta no terminal eu usei o CD seguido do nome da pasta ex: cdPasta.......

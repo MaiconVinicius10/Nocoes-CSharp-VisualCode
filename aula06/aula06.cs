65ea92d53db48afc4abb9a3e66bd905c61d4905e
Maicon Vinicius <maicon.vinicius.77920@gmail.com>
2023-06-13 05:24:50 -0300

    Atualizando Aula06

--- aula06/aula06.cs
+++ aula06/aula06.cs
@@ -9,28 +9,27 @@ class Aula06{
         int n1,n2,n3;
         //setando valores
         n1=10; n2=20; n3=30;
-        //exibindo valores // referindo as posições como indices 0 , 1 ,2
-        // o barra ena eel quebra a linha 
+        // o barra n quebra a linha 
         // barra t inseri tabulação
-
+        //exibindo valores // referindo as posições como indices 0 , 1 ,2
         Console.WriteLine("\nn1=\t{0} \nn2=\t{1} \nn3=\t{2}",n1,n2,n3);
 
 
         // exemplo 2
-        //criando variaveis
+        //criando variaveis e setando valores
         double valorCompra=5.50;
         double valorVenda;
         double lucro=0.1;
         string produto="Pastel";
         //calculando meu valor de venda
         valorVenda=valorCompra+(valorCompra*lucro);
-        //usando tabulação com espaçamento 15 tamanho do espaço
+        //usando tabulação com espaçamento 15 de tamanho
         Console.WriteLine("Produto......{0,15}",produto);
-        //valor monetário de cifrão
+        //valor monetário de cifrão com espaçamento 15
         Console.WriteLine("Val.Compra....{0,15:c}",valorCompra);
-        //colocar porcentagem
+        //colocar porcentagem com espaçamento 15
         Console.WriteLine("Lucro.........{0,15:p}",lucro);
-        //colocar porcentagem
+        //colocar porcentagem com espaçamento 15
         Console.WriteLine("ValorVenda....{0,15:c}",valorVenda);
     }
 }

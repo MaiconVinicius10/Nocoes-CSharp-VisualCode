4cd4e9372ca74e8b22091d119a09be7f6bfc04fa
Maicon Vinicius <maicon.vinicius.77920@gmail.com>
2023-10-14 12:38:27 -0300

    treino 10
    Enumeradores

---
+++ aula10/treino10.cs
@@ -0,0 +1,23 @@
+// enumeradores
+//usando biblioteca do sistema 
+using System;
+
+//criando enumeradores
+enum DiasSemana{Domingo,Segunda,Terça,Quarta,Quinta,Sexta,Sabado};
+//criando classe
+class Aula10{
+    //criando metodo principal
+    static void Main(){
+        //instanciando forma 1//
+        //DiasSemana ds = DiasSemana.Domingo;
+        //
+        //intanciando forma2//
+        //DiasSemana ds = (DiasSemana)3;
+        //exibindo o resultado do ds
+
+        //intanciando forma 3 estou convertendo pra ter numero
+        int ds = (int)DiasSemana.Sexta;
+
+        Console.WriteLine(ds);
+    }
+}
